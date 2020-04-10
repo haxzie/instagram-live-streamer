d38aed8b943f07f263a18cad4f19cea2a76c7ad4
haxzie <musthu.gm@gmail.com>
2020-04-10 13:00:31 +0530

    remove electron forge

---
+++ forge.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  makers: [
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          maintainer: "haxzie",
+          homepage: "https://ehaxzie.com",
+        },
+      },
+    },
+  ],
+};
