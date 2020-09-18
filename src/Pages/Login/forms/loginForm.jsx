f640a420b70eb007cddb5b824662c975a1713e62
haxzie <musthu.gm@gmail.com>
2020-09-19 00:18:32 +0530

    Update version number

--- src/Pages/Login/forms/loginForm.jsx
+++ src/Pages/Login/forms/loginForm.jsx
@@ -47,7 +47,7 @@ export default function LoginForm({ handleLogin, credError }) {
         Login
       </Button>
       <div className={styles.statusTexts}>
-        <p className={styles.status}>Instagram Live Streamer v0.1.3</p>
+        <p className={styles.status}>Instagram Live Streamer v0.1.7-beta</p>
         <p className={styles.author}>
           Created by{" "}
           <span
